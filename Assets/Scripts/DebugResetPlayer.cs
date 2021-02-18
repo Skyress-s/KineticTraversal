90a2e26a7fda0aa38aec8bccdefe898d1859c43b
Skyress-s <mathmoe64@gmail.com>
2021-02-18 09:13:17 +0100

    small changes

---
+++ Assets/Scripts/DebugResetPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugResetPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = Player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Player.transform.position = Vector3.zero;
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
